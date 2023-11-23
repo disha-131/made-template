8bd9db7ee1e703b0825cb7de885c2a86062183c9
Disha Vinesh Jethva <disha.vinesh.jethva@fau.de>
2023-11-23 19:48:49 +0100

    small changes

--- project/pipeline.py
+++ project/pipeline.py
@@ -4,13 +4,18 @@ import pandas as pd
 import sqlite3
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def extract_zip(old_name: str, new_name: str):
+def extract_and_move(old_name: str, new_name: str):
     shutil.move(old_name, new_name)
     with zipfile.ZipFile(new_name, 'r') as zip_ref:
-        zip_ref.extractall('items')
+        zip_ref.extractall('items_zip')
 
-def read_and_preprocess(file_path, nan_fill_value=0, encoding='latin-1'):
-    data = pd.read_csv(file_path, encoding=encoding)
+def extract_and_move_evaluation(old_name: str, new_name: str):
+    shutil.move(old_name, new_name)
+    with zipfile.ZipFile(new_name, 'r') as zip_ref:
+        zip_ref.extractall('transactions_zip')
+
+def load_and_fill_missing(file_path, nan_fill_value=0, encoding='latin-1'):
+    data = pd.read_csv(file_path, encoding=encoding,delimiter='|')
     data.fillna(nan_fill_value, inplace=True)
     return data
 
@@ -23,20 +28,15 @@ def save_to_csv_and_sql(data, csv_path, sql_path, table_name):
 if __name__ == "__main__":
     api = KaggleApi()
     api.authenticate()
-
-    # download the datasets
-    api.dataset_download_file('oscarm524/book-recommendation/data','items.csv')
     
-    extract_zip('items.csv.zip', 'items.zip')
+    api.dataset_download_file('oscarm524/book-recommendation/data','items.csv')
+    api.dataset_download_file('oscarm524/book-recommendation/data','transactions.csv')
 
-    # read the both datasets
-    item_data = read_and_preprocess('items.csv')
-    transaction_data = read_and_preprocess('transactions.csv')
+    extract_and_move('items.csv.zip', 'items_zip.zip')
+    extract_and_move_evaluation('transactions.csv.zip', 'transactions_zip.zip')
 
-    # Read and preprocess Renewable Energy dataset
-    renewables_data = read_and_preprocess('transactions.csv', nan_fill_value=0)
-    transaction_data = transaction_data.rename(columns={'Entity': 'itemID'})
-
-    # Save datasets to CSV and SQLite
-    save_to_csv_and_sql(item_data, 'data/items.csv', 'data/item.sqlite', 'item')
-    save_to_csv_and_sql(transaction_data, 'data/transactions.csv', 'data/transaction.sqlite', 'transaction_data')
+    co2_data = load_and_fill_missing('items_zip/items.csv')
+    transactions_data = load_and_fill_missing('transactions_zip/transactions.csv')
+    
+    save_to_csv_and_sql(co2_data, 'data/items.csv', 'data/items_db.db', 'items')
+    save_to_csv_and_sql(transactions_data, 'data/transactions.csv', 'data/transactions_data.db', 'transactions_data')
